e0684e260ab5a2e129aee4b7d8f81289216b356f
AJBubb <aj@altifysoftware.com>
2014-06-02 14:14:17 -0400

    Initial Commit

---
+++ SendGridInboundParse/SendGridInboundParse/Controllers/InboundParseController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SendGridInboundParse.Controllers
+{
+    public class InboundParseController : ApiController
+    {
+        [HttpPost]
+        // POST api/<controller>
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<HttpResponseMessage> Post()
+        {
+            try
+            {
+                string root = HttpContext.Current.Server.MapPath("~/App_Data");
+                var provider = new MultipartFormDataStreamProvider(root);
+
+                StringBuilder sb = new StringBuilder(); // Holds the response body
+
+                // Read the form data and return an async task.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                //Bind to incoming email model
+                Models.InboundParseModel _em = new Models.InboundParseModel
+                {
+                    to = provider.FormData.GetValues("to").SingleOrDefault(),
+                    from = provider.FormData.GetValues("from").SingleOrDefault(),
+                    subject = provider.FormData.GetValues("subject").SingleOrDefault(),
+                    html = provider.FormData.GetValues("html").SingleOrDefault(),
+                    sender_ip = provider.FormData.GetValues("sender_ip").SingleOrDefault(),
+                    headers = provider.FormData.GetValues("headers").SingleOrDefault(),
+                    dkim = provider.FormData.GetValues("dkim").SingleOrDefault(),
+                    text = provider.FormData.GetValues("text").SingleOrDefault(),
+                    SPF = provider.FormData.GetValues("spf").SingleOrDefault(),
+                    attachments = provider.FormData.GetValues("attachments").SingleOrDefault(),
+                    envelope = provider.FormData.GetValues("envelope").SingleOrDefault(),
+                    charsets = provider.FormData.GetValues("charsets").SingleOrDefault()
+                };
+                return new HttpResponseMessage(HttpStatusCode.OK);
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ SendGridInboundParse/SendGridInboundParse/Models/InboundParseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendGridInboundParse.Models
+{
+    public class InboundParseModel
+    {
+        public string headers { get; set; }
+        public string dkim { get; set; }
+        public string to { get; set; }
+        public string html { get; set; }
+        public string from { get; set; }
+        public string text { get; set; }
+        public string sender_ip { get; set; }
+        public string SPF { get; set; }
+        public string attachments { get; set; }
+        public string subject { get; set; }
+        public string envelope { get; set; }
+        public string charsets { get; set; }
+    }
+}
